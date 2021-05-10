1fb30eeeb29d822bff4930440597edd7e59a5962
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-05-10 14:12:48 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const animal = document.querySelectorAll('.animal');
 const score = document.querySelector('.score');
 const attempt = document.querySelector('.attempt');
 const display = document.querySelector('.guessDisplay');
+const cardsHolder = document.querySelector('.cards');
+const levels = document.querySelector('#levels');
+const select = document.querySelector('select');
 
 let guessArr = [];
 let playerScore = 0;
@@ -31,24 +34,12 @@ let shuffle = (arr) => {
         arr[j] = temp;
     }
 }
-shuffle(nums);
 
 //Use randomised nums array to set the img path of a random animal
-for(let i = 0; i < nums.length; i++){
-    let randImg = nums[i];
-    animal[i].setAttribute('src', `img/${imgArr[randImg]}`);
-};
-
-//Adds an event listener to each card
-for(let i = 0; i < card.length; i++){
-    card[i].addEventListener('click', function(){
-        card[i].classList.toggle('card');
-        animal[i].classList.toggle('animalOn');
-        animal[i].parentElement.classList.toggle('unclick');
-        guessArr.push(animal[i]);
-        answerCheck();
-    })
-};
+// for(let i = 0; i < nums.length; i++){
+//     let randImg = nums[i];
+//     animal[i].setAttribute('src', `img/${imgArr[randImg]}`);
+// };
 
 //Check the two cards when both answers are added to the array
 const answerCheck = () => {
@@ -98,4 +89,62 @@ const cardReset = () => {
     }, 1000)
 };
 
+//TO DO - level functionality
+//make the divs depending on which level is selected
+//Use .slice to create an array
+//Change the level1,2,3 etc to an object?
 
+
+const levelSelect = (lvlSelect) => {
+    if(lvlSelect.value === '1'){
+        level1();
+    } else if (lvlSelect.value === '2'){
+        console.log('level 2');
+    } else if (lvlSelect.value === '3'){
+        console.log('level 3');
+    } else {
+        console.log('level 4');
+    }
+}
+
+select.addEventListener('change', function(){
+    levelSelect(levels);
+})
+
+const level1 = () => {
+    const lvl1arr = nums.slice(0,4);
+    const lvl1img = imgArr.slice(0,4);
+    shuffle(lvl1arr);
+    createCards(lvl1arr, lvl1img);
+    listenOnCards();
+}
+
+const createCards = (numarr, imgarr) => {
+    for(let i = 0; i < numarr.length; i++){
+        const div = document.createElement('div');
+        div.classList.add('card');
+        const img = document.createElement('img');
+        img.classList.add('animal');
+        let randImg = numarr[i];
+        img.setAttribute('src', `img/${imgarr[randImg]}`);
+        div.append(img);
+        cardsHolder.append(div);
+    }
+};
+
+let cardsInHolder = cardsHolder.childNodes;
+
+//Adds an event listener to each card
+//TO DO: SORT THIS LISTENER OUT?! Pass in imgarrays as params
+const listenOnCards = () => {
+    for(let i = 1; i <= cardsInHolder.length; i++){
+        cardsInHolder[i].addEventListener('click', function(){
+            // cardsInHolder[i].classList.toggle('card');
+            // animal[i].classList.toggle('animalOn');
+            // animal[i].parentElement.classList.toggle('unclick');
+            // guessArr.push(animal[i]);
+            // answerCheck();
+            alert('works');
+        })
+    };
+}
